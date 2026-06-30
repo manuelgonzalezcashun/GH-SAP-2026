296bbeada411890b28208f775482ab47f530cd86
Cool-Bot1 <145876030+Cool-Bot1@users.noreply.github.com>
2026-06-30 16:23:03 -0700

    attempt to fix playercontroller (fail)
    may need some help cause im more used to the old velocity systems

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3;
     public Rigidbody2D rb;
+    float horiz;
+    float vert;
+
+    void Start()
+    {
+        
+    }
 
     void FixedUpdate()
     {
-        float horiz = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        rb.linearVelocityX = new Vector2(horiz * speed, vert * speed);
+        rb.linearVelocityY = new Vector2(horiz * speed, vert * speed);
+        
+    }
 
-        Vector2 vectCheck = new Vector2(horiz,vert);
-       
-        rb.linearVelocity = vectCheck.normalized * speed;
-        
-        
-        
+    public void Move(InputAction.CallbackContext context)
+    {
+        horiz = context.ReadValue<Vector2>().x;
+        vert = context.ReadValue<Vector2>().y;
     }
 }
