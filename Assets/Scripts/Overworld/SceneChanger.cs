896b19237562e21641e4dace0359396cc4daf5f5
Cool-Bot1 <145876030+Cool-Bot1@users.noreply.github.com>
2026-07-08 16:39:04 -0700

    Level Transitions
    -Level Transitions added
    -Level Transition is immediately up for the first scene and immediately down for the second
    -Need to work on saving location/angle entered better

---
+++ Assets/Scripts/Overworld/SceneChanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+}
