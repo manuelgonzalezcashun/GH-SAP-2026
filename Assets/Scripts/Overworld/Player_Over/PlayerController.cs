e17f12bfd03fee7376668f76953f26caaddcc382
Manuel Gonzalez Cashun <99101054+manuelgonzalezcashun@users.noreply.github.com>
2026-07-31 21:54:02 -0700

    Player Animations Added

--- Assets/Scripts/Overworld/Player_Over/PlayerController.cs
+++ Assets/Scripts/Overworld/Player_Over/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,9 +7,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float interactDistance = 1.5f;
     [SerializeField] float speed = 3;
     private bool canMove = true;
-
     private static float SpawnpointX;
     private static float SpawnpointY;
+    private Animator animator;
+    private int currentState;
 
     void OnEnable()
     {
@@ -21,6 +23,10 @@ public class PlayerController : MonoBehaviour
         EventBus.UnSubscribe<SceneTransition>(EntryPoint);
         EventBus.UnSubscribe<PlayerMoveEvent>(CanPlayerMove);
     }
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Start()
     {
@@ -32,6 +38,12 @@ public class PlayerController : MonoBehaviour
         if (InputHandler.InteractPressed) PlayerInteract();
 
         EventBus.Raise(new ItemSearchEvent { _interactDistance = interactDistance, _interactPosition = transform.position });
+
+        int state = GetState();
+        if (currentState == state) return;
+
+        animator.CrossFade(state, 0, 0);
+        currentState = state;
     }
     void FixedUpdate()
     {
@@ -41,6 +53,16 @@ public class PlayerController : MonoBehaviour
     {
         if (canMove)
             rb.linearVelocity = InputHandler.Movement.normalized * speed;
+
+        PlayerFlip();
+    }
+    private void PlayerFlip()
+    {
+        int flip_X = 1;
+        if (InputHandler.Movement.x > 0) flip_X = -1;
+        if (InputHandler.Movement.x < 0) flip_X = 1;
+
+        transform.localScale = new Vector3(flip_X, transform.localScale.y);
     }
     void PlayerInteract()
     {
@@ -58,4 +80,22 @@ public class PlayerController : MonoBehaviour
         canMove = data.canMove;
         rb.linearVelocity = Vector2.zero;
     }
+
+    private int GetState()
+    {
+        if (InputHandler.Movement.y < 0) return AnimationState.WalkSouth;
+        if (InputHandler.Movement.y > 0) return AnimationState.WalkNorth;
+        if (InputHandler.Movement.x != 0) return AnimationState.WalkSide;
+        if (InputHandler.Movement == Vector2.zero) return AnimationState.Idle;
+
+        return -1;
+    }
+}
+
+public struct AnimationState
+{
+    public static readonly int WalkNorth = Animator.StringToHash("PlayerWalkNorth");
+    public static readonly int WalkSouth = Animator.StringToHash("PlayerWalkSouth");
+    public static readonly int WalkSide = Animator.StringToHash("PlayerWalkSide");
+    public static readonly int Idle = Animator.StringToHash("PlayerIdle");
 }
