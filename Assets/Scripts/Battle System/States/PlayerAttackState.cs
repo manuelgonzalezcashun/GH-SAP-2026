1500860283856a8d23aa6e966b805118f3215b69
Manuel Gonzalez Cashun <99101054+manuelgonzalezcashun@users.noreply.github.com>
2026-08-12 19:01:43 -0700

    Resetting Button Selection for Battle System

--- Assets/Scripts/Battle System/States/PlayerAttackState.cs
+++ Assets/Scripts/Battle System/States/PlayerAttackState.cs
@@ -65,7 +65,6 @@ public class PlayerAttackState : BattleState
             else
                 _system.StartCoroutine(Heal());
 
-            Debug.Log($"Target Confirmed! Current Selection: {_selectedIndex}");
             _selectedIndex = -1;
             return;
         }
