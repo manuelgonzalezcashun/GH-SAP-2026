7203297afce61f407744563939de114b869a66d5
Manuel Gonzalez Cashun <99101054+manuelgonzalezcashun@users.noreply.github.com>
2026-08-14 15:51:26 -0700

    Minor changes to scene
    - Enabled all NPCs in scenes
    - removed items from Player's inventory
    - Added Sprite Renderer on the Collectable prefab

--- Assets/Scripts/Interactables/Collectable.cs
+++ Assets/Scripts/Interactables/Collectable.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Collectable : Interactable
 {
     [SerializeField] SO_Item itemData = null;
+
+    SpriteRenderer spriteRenderer = null;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = itemData.Item.Sprite;
+    }
     public override void OnInteract()
     {
         // If Item isn't null, add it to the inventory
